7da7ea36f3f97b836a5f4147a3fbc6b99f58dfae
Tom Kennedy <tkennedy@pivotal.io>
2019-05-20 17:59:59 -0400

    use unix domain socket instead of http
    [165568088]
    
    Co-authored-by: Mark DeLillo <mdelillo@pivotal.io>
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- backends/remote/src/main/kotlin/org/cloudfoundry/credhub/remote/RemoteBackendClient.kt
+++ backends/remote/src/main/kotlin/org/cloudfoundry/credhub/remote/RemoteBackendClient.kt
@@ -1,8 +1,18 @@
 package org.cloudfoundry.credhub.remote
 
 import io.grpc.internal.GrpcUtil
-import io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.NegotiationType
 import io.grpc.netty.NettyChannelBuilder
+import io.netty.channel.Channel
+import io.netty.channel.EventLoopGroup
+import io.netty.channel.epoll.Epoll
+import io.netty.channel.epoll.EpollDomainSocketChannel
+import io.netty.channel.epoll.EpollEventLoopGroup
+import io.netty.channel.kqueue.KQueue
+import io.netty.channel.kqueue.KQueueDomainSocketChannel
+import io.netty.channel.kqueue.KQueueEventLoopGroup
+import io.netty.channel.unix.DomainSocketAddress
+import org.apache.logging.log4j.LogManager
 import org.cloudfoundry.credhub.ErrorMessages
 import org.cloudfoundry.credhub.exceptions.EntryNotFoundException
 import org.cloudfoundry.credhub.remote.grpc.CredentialServiceGrpc
@@ -10,29 +20,33 @@ import org.cloudfoundry.credhub.remote.grpc.GetByIdRequest
 import org.cloudfoundry.credhub.remote.grpc.GetByNameRequest
 import org.cloudfoundry.credhub.remote.grpc.GetResponse
 import org.springframework.stereotype.Service
-import java.io.File
 import java.util.concurrent.TimeUnit
 
 
 @Service
-class RemoteBackendClient(
-    remoteTlsConfig: RemoteTlsConfig
-) {
+class RemoteBackendClient {
 
-    private final val caFile = File(remoteTlsConfig.trustedCa)
-    private final val keyFile = File(remoteTlsConfig.clientPrivateKey)
-    private final val certFile = File(remoteTlsConfig.clientCertificate)
+    companion object {
+        private val LOGGER = LogManager.getLogger(RemoteBackendClient::class.java)
+    }
 
-    var blockingStub: CredentialServiceGrpc.CredentialServiceBlockingStub = CredentialServiceGrpc.newBlockingStub(
-        NettyChannelBuilder.forAddress("localhost", 10000)
-            .useTransportSecurity()
-            .sslContext(GrpcSslContexts
-                .forClient()
-                .trustManager(caFile)
-                .keyManager(certFile, keyFile)
+    private lateinit var group: EventLoopGroup
+    private lateinit var channelType: Class<out Channel>
+    private lateinit var blockingStub: CredentialServiceGrpc.CredentialServiceBlockingStub
+
+
+
+    init {
+        setChannelInfo()
+        blockingStub = CredentialServiceGrpc.newBlockingStub(
+            //todo: move socket address to application.yaml
+            NettyChannelBuilder.forAddress(DomainSocketAddress("/tmp/socket/test.sock"))
+                .eventLoopGroup(group)
+                .channelType(channelType)
+                .negotiationType(NegotiationType.PLAINTEXT)
+                .keepAliveTime(GrpcUtil.DEFAULT_KEEPALIVE_TIME_NANOS, TimeUnit.NANOSECONDS)
                 .build())
-            .keepAliveTime(GrpcUtil.DEFAULT_KEEPALIVE_TIME_NANOS, TimeUnit.NANOSECONDS)
-            .build())
+    }
 
     fun getByNameRequest(credentialName: String, user: String): GetResponse {
         val request = GetByNameRequest
@@ -68,5 +82,24 @@ class RemoteBackendClient(
         }
 
         return getResponse
+    }
+
+    private fun setChannelInfo() {
+        when {
+            Epoll.isAvailable() -> {
+                group = EpollEventLoopGroup()
+                channelType = EpollDomainSocketChannel::class.java
+                LOGGER.info("Using epoll for Netty transport.")
+            }
+            KQueue.isAvailable() -> {
+                group = KQueueEventLoopGroup()
+                channelType = KQueueDomainSocketChannel::class.java
+                LOGGER.info("Using KQueue for Netty transport.")
+            }
+            else -> {
+                //todo: look into nio as a default option
+                throw RuntimeException("Unsupported OS '" + System.getProperty("os.name") + "', only Unix and Mac are supported")
+            }
+        }
     }
 }
