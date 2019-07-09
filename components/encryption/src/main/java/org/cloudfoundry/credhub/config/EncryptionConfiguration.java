5fb0b817ba86d2b476883b7a8c179ad43ad07459
Mark DeLillo <mdelillo@pivotal.io>
2019-07-09 15:39:58 -0400

    Implement TLS on KMS gRPC communication layer
    [166792592]
    
    We now encrypt the traffic over the socket between the gRPC client and
    server. This requires configuring the `kms-provider` plugin with the CA
    used to sign the certificate being used on the kms server side, as well
    as the hostname the certificate is signed for.
    
    Signed-off-by: David Lohle <Proplex@users.noreply.github.com>

--- components/encryption/src/main/java/org/cloudfoundry/credhub/config/EncryptionConfiguration.java
+++ components/encryption/src/main/java/org/cloudfoundry/credhub/config/EncryptionConfiguration.java
@@ -2,13 +2,16 @@ package org.cloudfoundry.credhub.config;
 
 public class EncryptionConfiguration {
   private Integer port;
-  private String host;
   private String partition;
   private String partitionPassword;
   private String serverCa;
   private String clientCertificate;
   private String clientKey;
+
+  private String host;
+
   private String endpoint;
+  private String ca;
 
   public String getServerCa() {
     return serverCa;
@@ -70,18 +73,30 @@ public class EncryptionConfiguration {
     return endpoint;
   }
 
-  public void setEndpoint(final String endpoint) {
+  public void setEndpoint(String endpoint) {
     this.endpoint = endpoint;
+  }
+
+  public String getCa() {
+    return ca;
+  }
+
+  public void setCa(String ca) {
+    this.ca = ca;
   }
 
   @Override
   public String toString() {
     return "EncryptionConfiguration{" +
       "port=" + port +
-      ", host='" + host + '\'' +
       ", partition='" + partition + '\'' +
       ", partitionPassword='" + partitionPassword + '\'' +
+      ", serverCa='" + serverCa + '\'' +
+      ", clientCertificate='" + clientCertificate + '\'' +
+      ", clientKey='" + clientKey + '\'' +
+      ", host='" + host + '\'' +
       ", endpoint='" + endpoint + '\'' +
+      ", ca='" + ca + '\'' +
       '}';
   }
 }
